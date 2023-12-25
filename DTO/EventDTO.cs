eb2f7d0602515324117d198f819a300fab3ceed6
תמר <@DESKTOP-7288C0Q>
2023-12-25 20:30:45 +0200

    Calendar

---
+++ DTO/EventDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Learning_Space.DTO
+{
+    public class EventDTO
+    {
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+
+}
