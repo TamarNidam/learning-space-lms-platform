92318be4d55d60424af93de7cef8d404d0b892bf
TamarNidam <tamtam2003n@gmail.com>
2024-01-03 00:16:53 +0200

    message view

--- DTO/MassageDTO.cs
+++ DTO/MassageDTO.cs
@@ -1,6 +1,18 @@
-﻿namespace Learning_Space.DTO
+﻿using Learning_Space.Models;
+
+namespace Learning_Space.DTO
 {
     public class MassageDTO
     {
+        public int MessageId { get; set; }
+
+        public int? CourseId { get; set; }
+
+        public string MassageSubject { get; set; }
+
+        public DateTime? MassageDateTime { get; set; }
+
+        public string Content { get; set; }
+
     }
 }
