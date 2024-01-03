26b887e6d4c7b47afc041a7ff5aefc454dfc806a
TamarNidam <tamtam2003n@gmail.com>
2024-01-03 02:00:48 +0200

    message controller. its workkk

--- Controllers/MassagesController.cs
+++ Controllers/MassagesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Learning_Space.Models;
+using Learning_Space.DTO;
 
 namespace Learning_Space.Controllers
 {
@@ -19,29 +20,48 @@ namespace Learning_Space.Controllers
         }
 
         // GET: Massages
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? courseid)
         {
-            var learningSpaceContext = _context.Massages.Include(m => m.Course);
-            return View(await learningSpaceContext.ToListAsync());
+            if (!courseid.HasValue)
+            {
+                return NotFound();
+            }
+            var message = await _context.Massages
+             .Where(m => m.CourseId == courseid)
+             .Select(u => new MassageDTO
+             {
+                 MessageId = u.MessageId,
+                 CourseId = u.CourseId,
+                 MassageSubject = u.MassageSubject,
+                 MassageDateTime = u.MassageDateTime,
+                 Content = u.Content
+             }).ToListAsync();
+            return View(message);
         }
 
         // GET: Massages/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details(int? messageid)
         {
-            if (id == null)
+            if (messageid == null)
             {
                 return NotFound();
             }
 
-            var massage = await _context.Massages
-                .Include(m => m.Course)
-                .FirstOrDefaultAsync(m => m.MessageId == id);
+            var massage = await _context.Massages.FirstOrDefaultAsync(m => m.MessageId == messageid);
             if (massage == null)
             {
                 return NotFound();
             }
+            var messageDTO = new MassageDTO
+            {
+                MessageId = massage.MessageId,
+                CourseId = massage.CourseId,
+                MassageSubject = massage.MassageSubject,
+                MassageDateTime = massage.MassageDateTime,
+                Content = massage.Content
+            };
 
-            return View(massage);
+            return View(messageDTO);
         }
 
         // GET: Massages/Create
@@ -56,43 +76,52 @@ namespace Learning_Space.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("MessageId,CourseId,MassageSubject,MassageDateTime,Content")] Massage massage)
+        public async Task<IActionResult> Create(int user, int permission, int courseid, [Bind("MessageId,CourseId,MassageSubject,MassageDateTime,Content")] MassageDTO massage)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(massage);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                var maxId = await _context.Massages.MaxAsync(u => (int?)u.MessageId) ?? 0;
+                var newId = maxId + 1;
+                var sql = $"INSERT INTO [Massages] (MessageId,CourseId,MassageSubject,MassageDateTime,Content) VALUES ({newId}, {courseid},'{massage.MassageSubject}', GETDATE(), '{massage.Content}')";
+                await _context.Database.ExecuteSqlRawAsync(sql);
+                return Redirect($"/Massages/Index?user={user}&permission={permission}&courseid={courseid}");
             }
-           
+
             return View(massage);
         }
 
         // GET: Massages/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public async Task<IActionResult> Edit(int? messageid)
         {
-            if (id == null)
+            if (messageid == null)
             {
                 return NotFound();
             }
 
-            var massage = await _context.Massages.FindAsync(id);
+            var massage = await _context.Massages.FindAsync(messageid);
             if (massage == null)
             {
                 return NotFound();
             }
-            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseName", massage.CourseId);
-            return View(massage);
+            var messageDTO = new MassageDTO
+            {
+                MessageId = massage.MessageId,
+                CourseId = massage.CourseId,
+                MassageSubject = massage.MassageSubject,
+                MassageDateTime = massage.MassageDateTime,
+                Content = massage.Content
+            };
+            
+            return View(messageDTO);
         }
 
         // POST: Massages/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("MessageId,CourseId,MassageSubject,MassageDateTime,Content")] Massage massage)
+        public async Task<IActionResult> Edit(int user, int permission, int courseid, int messageid, [Bind("MessageId,CourseId,MassageSubject,MassageDateTime,Content")] MassageDTO massage)
         {
-            if (id != massage.MessageId)
+            if (messageid != massage.MessageId)
             {
                 return NotFound();
             }
@@ -101,7 +130,15 @@ namespace Learning_Space.Controllers
             {
                 try
                 {
-                    _context.Update(massage);
+                    var messageDTO = new Massage
+                    {
+                        MessageId = massage.MessageId,
+                        CourseId = courseid,
+                        MassageSubject = massage.MassageSubject,
+                        MassageDateTime = massage.MassageDateTime,
+                        Content = massage.Content
+                    };
+                    _context.Update(messageDTO);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
@@ -115,44 +152,27 @@ namespace Learning_Space.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                return Redirect($"/Massages/Details?user={user}&permission={permission}&courseid={courseid}&messageid={messageid}");
             }
-            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseName", massage.CourseId);
             return View(massage);
         }
 
-        // GET: Massages/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var massage = await _context.Massages
-                .Include(m => m.Course)
-                .FirstOrDefaultAsync(m => m.MessageId == id);
-            if (massage == null)
-            {
-                return NotFound();
-            }
-
-            return View(massage);
-        }
+       
 
         // POST: Massages/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        [HttpPost]
+       
+        public async Task<IActionResult> Deletey(int user, int permission, int courseid, int messageid)
         {
-            var massage = await _context.Massages.FindAsync(id);
+            var massage = await _context.Massages.FindAsync(messageid);
             if (massage != null)
             {
                 _context.Massages.Remove(massage);
             }
 
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return Redirect($"/Massages/Index?user={user}&permission={permission}&courseid={courseid}");
+
         }
 
         private bool MassageExists(int id)
