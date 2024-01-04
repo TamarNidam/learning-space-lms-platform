959e9636ea28e2778ef77440e7b5fd25b616ff42
TamarNidam <tamtam2003n@gmail.com>
2024-01-04 18:15:37 +0200

    lesson views

---
+++ DTO/LessonDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Policy;
+
+namespace Learning_Space.DTO
+{
+    public class LessonDTO
+    {
+        public int LessonId { get; set; }
+
+        public int? CourseId { get; set; }
+
+        public string? CourseName { get; set; }
+
+        public string LessonSubject { get; set; }
+
+        public DateOnly LessonDate { get; set; }
+
+        public TimeOnly? StartTime { get; set; }
+
+        public TimeOnly? EndTime { get; set; }
+
+        public string LessonType { get; set; }
+
+        public Url? ZoomUrl { get; set; }
+    }
+}
