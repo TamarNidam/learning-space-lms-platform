72543bb4fd2168ca8ac6feaa1e60a6df2c1083b7
TamarNidam <tamtam2003n@gmail.com>
2024-01-09 07:28:33 +0200

    lessons

--- Controllers/LessonsController.cs
+++ Controllers/LessonsController.cs
@@ -33,6 +33,7 @@ namespace Learning_Space.Controllers
             var lessons = await _context.Lessons
     .Where(m => m.CourseId == courseid && m.LessonDate >= currentDate)
     .OrderBy(m => m.LessonDate)
+    .ThenBy(m => m.StartTime)
     .Join(
         _context.Courses,
         l => l.CourseId,
@@ -93,7 +94,7 @@ namespace Learning_Space.Controllers
         // GET: Lessons/Create
         public IActionResult Create()
         {
-           
+
             return View();
         }
 
@@ -107,23 +108,23 @@ namespace Learning_Space.Controllers
             if (ModelState.IsValid)
             {
                 DateOnly today = DateOnly.FromDateTime(DateTime.Today);
-                
 
-                if (lesson.LessonDate > today && lesson.StartTime > TimeOnly.FromDateTime(DateTime.Now))
+
+                if (lesson.LessonDate < today || lesson.LessonDate == today && lesson.StartTime < TimeOnly.FromDateTime(DateTime.Now))
                 {
                     ViewBag.ErrorMessage = "A class can only be scheduled for a date that has not yet passed.";
                     return View(lesson);
                 }
                 var clas = _context.CourseInClasses.FirstOrDefault(c => c.CourseId == courseid).ClassId;
                 // Check if there are any lessons scheduled for the course within the specified time range
-                bool isCourseAvailable = await CheckCourseAvailability(clas, lesson.StartTime, lesson.EndTime);
+                bool isCourseAvailable = await CheckCourseAvailability(clas, lesson.LessonDate, lesson.StartTime, lesson.EndTime);
 
                 // Check if the teacher has any lessons scheduled within the specified time range for the course
-                bool isTeacherAvailable = await CheckTeacherAvailability(courseid, lesson.StartTime, lesson.EndTime);
+                bool isTeacherAvailable = await CheckTeacherAvailability(courseid, lesson.LessonDate, lesson.StartTime, lesson.EndTime);
 
                 if (!isCourseAvailable)
                 {
-                    ViewBag.ErrorMessage =  "There is already a lesson scheduled for the course within the specified time range.";
+                    ViewBag.ErrorMessage = "There is already a lesson scheduled for the course within the specified time range.";
                     return View(lesson);
                 }
 
@@ -135,16 +136,29 @@ namespace Learning_Space.Controllers
 
                 var maxId = await _context.Lessons.MaxAsync(u => (int?)u.LessonId) ?? 0;
                 var newId = maxId + 1;
-              
+
                 var sql = $"INSERT INTO [Lessons] (LessonId,CourseId,LessonSubject,LessonDate,StartTime,EndTime,LessonType) VALUES ({newId}, {courseid},'{lesson.LessonSubject}', '{lesson.LessonDate}', '{lesson.StartTime}', '{lesson.EndTime}', '{lesson.LessonType}')";
                 await _context.Database.ExecuteSqlRawAsync(sql);
-               
-                if(lesson.LessonType == "Zoom")
+
+                if (lesson.LessonType == "Zoom")
                 {
                     var maxIdzoom = await _context.ZoomLessons.MaxAsync(u => (int?)u.ZoomLesson1) ?? 0;
                     var newIdzoom = maxIdzoom + 1;
-                     sql = $"INSERT INTO [ZoomLessons] (ZoomLesson,LessonId,ZoomUrl) VALUES ({newIdzoom}, {newId}, '{lesson.ZoomUrl}')";
+                    sql = $"INSERT INTO [ZoomLessons] (ZoomLesson,LessonId,ZoomUrl) VALUES ({newIdzoom}, {newId}, '{lesson.ZoomUrl}')";
                     await _context.Database.ExecuteSqlRawAsync(sql);
+                }
+
+                var maxId3 = await _context.LessonAttends.MaxAsync(u => (int?)u.LessonAttendId) ?? 0;
+                var studentsInClass = _context.StudentInClasses
+       .Where(s => s.ClassId == clas)
+       .Select(s => s.UserId)
+       .ToList();
+                foreach (int UserId in studentsInClass)
+                {
+                    maxId3 = maxId3 + 1;
+                    sql = $"INSERT INTO [LessonAttend] (LessonAttendId,LessonId,UserId,Attend) VALUES ({maxId3}, {newId},{UserId},{0})";
+                    await _context.Database.ExecuteSqlRawAsync(sql);
+
                 }
 
                 return Redirect($"/Lessons/Index?user={user}&permission={permission}&courseid={courseid}");
@@ -152,43 +166,75 @@ namespace Learning_Space.Controllers
             return View(lesson);
         }
 
-        private async Task<bool> CheckCourseAvailability(int? classId, TimeOnly startTime, TimeOnly endTime)
+        private async Task<bool> CheckCourseAvailability(int? classId, DateOnly date, TimeOnly startTime, TimeOnly endTime)
         {
             // Get the list of CourseIds for the given ClassId
-            var courseIds = await _context.CourseInClasses.Where(cic => cic.ClassId == classId).Select(cic => cic.CourseId).ToListAsync();
+            var courseIds = await _context.CourseInClasses
+                .Where(cic => cic.ClassId == classId)
+                .Select(cic => cic.CourseId)
+                .ToListAsync();
+
             // Check if there are any lessons scheduled for the course within the specified time range
-            bool isAvailable = await _context.Lessons.AnyAsync(l => courseIds.Contains(l.CourseId) && l.StartTime <= endTime && l.EndTime >= startTime);
+            bool isAvailable = await _context.Lessons.AnyAsync(l =>
+            courseIds.Contains(l.CourseId) &&
+            l.LessonDate == date &&
+            l.StartTime <= endTime &&
+            l.EndTime >= startTime);
+
             return !isAvailable;
         }
 
-        private async Task<bool> CheckTeacherAvailability(int courseId, TimeOnly startTime, TimeOnly endTime)
+        private async Task<bool> CheckTeacherAvailability(int courseId, DateOnly date, TimeOnly startTime, TimeOnly endTime)
         {
             //Get the teacher
-            var teacher =await  _context.Teachers.FirstOrDefaultAsync(m => m.CourseId == courseId);
+            var teacher = await _context.Teachers
+                .FirstOrDefaultAsync(m => m.CourseId == courseId);
 
             // Get the list of CourseIds for the given ClassId
-            var courseIds = await _context.Teachers.Where(t => t.TeacherId == teacher.TeacherId).Select(t => t.CourseId).ToListAsync();
+            var courseIds = await _context.Teachers
+                .Where(t => t.TeacherId == teacher.TeacherId)
+                .Select(t => t.CourseId)
+                .ToListAsync();
 
             // Check if the teacher has any lessons scheduled within the specified time range for the course
-            bool isAvailable = await _context.Lessons.AnyAsync(l => courseIds.Contains(l.CourseId) && l.StartTime <= endTime && l.EndTime >= startTime);
+            bool isAvailable = await _context.Lessons.AnyAsync(l =>
+            courseIds.Contains(l.CourseId) &&
+             l.LessonDate == date &&
+            l.StartTime <= endTime &&
+            l.EndTime >= startTime);
+
             return !isAvailable;
         }
 
         // GET: Lessons/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public async Task<IActionResult> Edit(int? lessonid)
         {
-            if (id == null)
+            if (lessonid == null)
             {
                 return NotFound();
             }
 
-            var lesson = await _context.Lessons.FindAsync(id);
-            if (lesson == null)
+            var l = await _context.Lessons.FindAsync(lessonid);
+            if (l == null)
             {
                 return NotFound();
             }
-            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseName", lesson.CourseId);
-            return View(lesson);
+            var courseName = _context.Courses.FirstOrDefault(c => c.CourseId == l.CourseId).CourseName;
+
+            var lessonDTO = new LessonDTO
+            {
+                LessonId = l.LessonId,
+                CourseId = l.CourseId,
+                CourseName = courseName,
+                LessonSubject = l.LessonSubject,
+                LessonDate = l.LessonDate,
+                StartTime = (TimeOnly)l.StartTime,
+                EndTime = (TimeOnly)l.EndTime,
+                LessonType = l.LessonType,
+                ZoomUrl = l.LessonType == "Zoom" ? _context.ZoomLessons.FirstOrDefault().ZoomUrl : null
+            };
+
+            return View(lessonDTO);
         }
 
         // POST: Lessons/Edit/5
@@ -196,19 +242,60 @@ namespace Learning_Space.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("LessonId,CourseId,LessonSubject,LessonDate,StartTime,EndTime,LessonType")] Lesson lesson)
+        public async Task<IActionResult> Edit(int user, int permission, int courseid, int lessonid, [Bind("LessonId,CourseId,LessonSubject,LessonDate,StartTime,EndTime,LessonType,ZoomUrl")] LessonDTO lesson)
         {
-            if (id != lesson.LessonId)
+            if (lessonid != lesson.LessonId)
             {
                 return NotFound();
             }
 
             if (ModelState.IsValid)
             {
-                try
+                DateOnly today = DateOnly.FromDateTime(DateTime.Today);
+
+
+                if (lesson.LessonDate < today || lesson.LessonDate == today && lesson.StartTime < TimeOnly.FromDateTime(DateTime.Now))
                 {
-                    _context.Update(lesson);
+                    ViewBag.ErrorMessage = "A class can only be scheduled for a date that has not yet passed.";
+                    return View(lesson);
+                }
+                var clas = _context.CourseInClasses.FirstOrDefault(c => c.CourseId == courseid).ClassId;
+                // Check if there are any lessons scheduled for the course within the specified time range
+                bool isCourseAvailable = await CheckCourseAvailability(clas, lesson.LessonDate, lesson.StartTime, lesson.EndTime);
+
+                // Check if the teacher has any lessons scheduled within the specified time range for the course
+                bool isTeacherAvailable = await CheckTeacherAvailability(courseid, lesson.LessonDate, lesson.StartTime, lesson.EndTime);
+
+                if (!isCourseAvailable)
+                {
+                    ViewBag.ErrorMessage = "There is already a lesson scheduled for the course within the specified time range.";
+                    return View(lesson);
+                }
+
+                if (!isTeacherAvailable)
+                {
+                    ViewBag.ErrorMessage = "The teacher already has lessons scheduled within the specified time range for the course.";
+                }
+                    try
+                    {
+                    var lessonDTO = new Lesson
+                    {
+                        LessonId = lesson.LessonId,
+                        CourseId = lesson.CourseId,
+                        LessonSubject = lesson.LessonSubject,
+                        LessonDate = lesson.LessonDate,
+                        StartTime = (TimeOnly)lesson.StartTime,
+                        EndTime = (TimeOnly)lesson.EndTime,
+                        LessonType = lesson.LessonType
+                    };
+                    _context.Update(lessonDTO);
                     await _context.SaveChangesAsync();
+
+                    if (lesson.LessonType == "Zoom")
+                    {
+                        var sql = $"UPDATE [ZoomLessons] SET ZoomUrl = '{lesson.ZoomUrl}' WHERE LessonId = {lessonid}";
+                        await _context.Database.ExecuteSqlRawAsync(sql);
+                    }
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -221,44 +308,25 @@ namespace Learning_Space.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                return Redirect($"/Lessons/Index?user={user}&permission={permission}&courseid={courseid}");
             }
-            ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseName", lesson.CourseId);
             return View(lesson);
         }
 
-        // GET: Lessons/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var lesson = await _context.Lessons
-                .Include(l => l.Course)
-                .FirstOrDefaultAsync(m => m.LessonId == id);
-            if (lesson == null)
-            {
-                return NotFound();
-            }
-
-            return View(lesson);
-        }
 
         // POST: Lessons/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        [HttpPost]
+
+        public async Task<IActionResult> Deletey(int user, int permission, int courseid, int lessonid)
         {
-            var lesson = await _context.Lessons.FindAsync(id);
+            var lesson = await _context.Lessons.FindAsync(lessonid);
             if (lesson != null)
             {
                 _context.Lessons.Remove(lesson);
             }
 
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return Redirect($"/Lessons/Index?user={user}&permission={permission}&courseid={courseid}");
         }
 
         private bool LessonExists(int id)
