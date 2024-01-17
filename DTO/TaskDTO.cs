17b38f29343166c42fc76a0f6eeb15ce0c7f8505
TamarNidam <tamtam2003n@gmail.com>
2024-01-17 02:40:18 +0200

    task index files tasks

--- DTO/TaskDTO.cs
+++ DTO/TaskDTO.cs
@@ -1,4 +1,5 @@
 ﻿using Learning_Space.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Learning_Space.DTO
 {
@@ -7,13 +8,20 @@ namespace Learning_Space.DTO
         public int TaskId { get; set; }
 
         public string TaskType { get; set; }
-
+        [Required]
         public DateOnly? StartDate { get; set; }
-
+        [Required]
         public DateOnly EndDate { get; set; }
 
         public int? CourseId { get; set; }
 
-        public string CourseName { get; set; }
+        public string? CourseName { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Context {  get; set; }
+
+        public int Done { get; set; } =0;
+        public string? PerformanceContent { get; set; }
     }
 }
