f988b84eeec8e5489d54d825b1752cdaaf6bcecd
TamarNidam <tamtam2003n@gmail.com>
2024-03-10 21:04:49 +0200

    index users task for task/ teacher

--- Controllers/UserTasksController.cs
+++ Controllers/UserTasksController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Learning_Space.Models;
+using Learning_Space.DTO;
+using MyFile = System.IO.File;
 
 namespace Learning_Space.Controllers
 {
@@ -19,60 +21,56 @@ namespace Learning_Space.Controllers
         }
 
         // GET: UserTasks
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int taskid)
         {
-            var learningSpaceContext = _context.UserTasks.Include(u => u.Task).Include(u => u.User);
-            return View(await learningSpaceContext.ToListAsync());
-        }
-
-        // GET: UserTasks/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
+            try
             {
-                return NotFound();
-            }
-
-            var userTask = await _context.UserTasks
-                .Include(u => u.Task)
-                .Include(u => u.User)
-                .FirstOrDefaultAsync(m => m.UserTaskId == id);
-            if (userTask == null)
+                string subject = GetSubject(taskid);
+                var userTasks = await _context.UserTasks
+            .Where(t => t.TaskId == taskid)
+            .Include(t => t.User)
+            .Select(u => new UserTaskDTO
             {
-                return NotFound();
+                UserTaskId = u.UserTaskId,
+                UserId = u.UserId,
+                UserName = u.User.FirstName +" "+ u.User.LastName,
+                TaskId = u.TaskId,
+                TaskSubject = subject,
+                Mark = u.Mark,
+                Remarks = u.Remarks,
+                Done = u.Done
+            })
+            .ToListAsync();
+                return View(userTasks);
             }
-
-            return View(userTask);
-        }
-
-        // GET: UserTasks/Create
-        public IActionResult Create()
-        {
-            ViewData["TaskId"] = new SelectList(_context.Tasks, "TaskId", "TaskId");
-            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "FirstName");
-            return View();
-        }
-
-        // POST: UserTasks/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("UserTaskId,UserId,TaskId,Mark,Remarks,Done")] UserTask userTask)
-        {
-            if (ModelState.IsValid)
+            catch (Exception ex)
             {
-                _context.Add(userTask);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return View(ex);
             }
-            ViewData["TaskId"] = new SelectList(_context.Tasks, "TaskId", "TaskId", userTask.TaskId);
-            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "FirstName", userTask.UserId);
-            return View(userTask);
         }
 
+        private string GetSubject(int taskId)
+        {
+            string filePath = Path.Combine(".", "TextFiles", "Tasks", "Tasks.txt");
+            string[] lines = MyFile.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+
+                if (parts.Length >= 3 && int.Parse(parts[0]) == taskId)
+                {
+                    return parts[1];
+
+                }
+            }
+            return null;
+        }
+
+
+       
         // GET: UserTasks/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public async Task<IActionResult> Edit(int? usertaskid)
         {
             if (id == null)
             {
@@ -126,41 +124,7 @@ namespace Learning_Space.Controllers
             return View(userTask);
         }
 
-        // GET: UserTasks/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var userTask = await _context.UserTasks
-                .Include(u => u.Task)
-                .Include(u => u.User)
-                .FirstOrDefaultAsync(m => m.UserTaskId == id);
-            if (userTask == null)
-            {
-                return NotFound();
-            }
-
-            return View(userTask);
-        }
-
-        // POST: UserTasks/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var userTask = await _context.UserTasks.FindAsync(id);
-            if (userTask != null)
-            {
-                _context.UserTasks.Remove(userTask);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
+      
         private bool UserTaskExists(int id)
         {
             return _context.UserTasks.Any(e => e.UserTaskId == id);
--- DTO/UserTaskDTO.cs
+++ DTO/UserTaskDTO.cs
@@ -1,6 +1,23 @@
-﻿namespace Learning_Space.DTO
+﻿using Learning_Space.Models;
+
+namespace Learning_Space.DTO
 {
     public class UserTaskDTO
     {
+        public int UserTaskId { get; set; }
+
+        public int? UserId { get; set; }
+
+        public string UserName { get; set; }    = string.Empty;
+
+        public int? TaskId { get; set; }
+        public string TaskSubject { get; set; }
+
+        public decimal? Mark { get; set; }
+
+        public string Remarks { get; set; }
+
+        public bool Done { get; set; }
+
     }
 }
